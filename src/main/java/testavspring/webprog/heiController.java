f82d898c5e1753e991e1e48a33ebfe6122b1ec31
Sigurd Skyrud <69970004+sigzugern@users.noreply.github.com>
2021-02-11 14:11:27 +0100

    nye ting

--- src/main/java/testavspring/webprog/heiController.java
+++ src/main/java/testavspring/webprog/heiController.java
@@ -1,5 +1,15 @@
 package testavspring.webprog;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class heiController {
 
+    @GetMapping
+    public String hei(String navn){
+        int lengde = navn.length();
+
+        return "Hei verden "+navn+"! Navnet ditt har "+lengde+" bokstaver!";
+    }
 }
